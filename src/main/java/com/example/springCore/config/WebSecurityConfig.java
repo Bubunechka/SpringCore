81f2111bfff8e2e9a3e66371d76a6dbf90158b0e
Bubunechka <110813295+Bubunechka@users.noreply.github.com>
2024-02-16 15:03:46 +0300

    Update WebSecurityConfig.java (замена кода сo Spring Baeldung)

--- src/main/java/com/example/springCore/config/WebSecurityConfig.java
+++ src/main/java/com/example/springCore/config/WebSecurityConfig.java
@@ -1,27 +1,32 @@
 package com.example.springCore.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig {
+public class CustomWebSecurityConfigurerAdapter {
 
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http.authorizeHttpRequests(expressionInterceptUrlRegistry ->
-//                        expressionInterceptUrlRegistry.requestMatchers("/securityNone").permitAll()
-//                                .anyRequest().authenticated())
-//                .httpBasic(httpSecurityHttpBasicConfigurer -> httpSecurityHttpBasicConfigurer.authenticationEntryPoint(authenticationEntryPoint));
-//        http.addFilterAfter(new CustomFilter(), BasicAuthenticationFilter.class);
-//        return http.build();
-//    }
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exeption{
-//
-//    }
-//
-//}
+    @Autowired private RestAuthenticationEntryPoint authenticationEntryPoint;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+          .inMemoryAuthentication()
+          .withUser("user1")
+          .password(passwordEncoder().encode("user1Pass"))
+          .authorities("ROLE_USER");
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(expressionInterceptUrlRegistry ->
+                        expressionInterceptUrlRegistry.requestMatchers("/securityNone").permitAll()
+                                .anyRequest().authenticated())
+            .httpBasic(httpSecurityHttpBasicConfigurer -> httpSecurityHttpBasicConfigurer.authenticationEntryPoint(authenticationEntryPoint));
+        http.addFilterAfter(new CustomFilter(), BasicAuthenticationFilter.class);
+        return http.build();
+    }
+    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
