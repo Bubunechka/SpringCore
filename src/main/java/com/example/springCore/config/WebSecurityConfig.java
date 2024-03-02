b643dd0f4f0d4a2c2d732ac3d0bd4f97e2120e81
Алёна Катанская <alyona.katanskaya@gmail.com>
2024-03-02 22:11:03 +0300

    переписан WebSecurityConfig

--- src/main/java/com/example/springCore/config/WebSecurityConfig.java
+++ src/main/java/com/example/springCore/config/WebSecurityConfig.java
@@ -1,27 +1,23 @@
-package com.example.springCore.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-@EnableWebSecurity
-public class WebSecurityConfig {
-
+//package com.example.springCore.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class WebSecurityConfig {
+//
 //    @Bean
 //    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http.authorizeHttpRequests(expressionInterceptUrlRegistry ->
-//                        expressionInterceptUrlRegistry.requestMatchers("/securityNone").permitAll()
-//                                .anyRequest().authenticated())
-//                .httpBasic(httpSecurityHttpBasicConfigurer -> httpSecurityHttpBasicConfigurer.authenticationEntryPoint(authenticationEntryPoint));
-//        http.addFilterAfter(new CustomFilter(), BasicAuthenticationFilter.class);
+////        http.authorizeHttpRequests(expressionInterceptUrlRegistry ->
+////                        expressionInterceptUrlRegistry.requestMatchers("/securityNone").permitAll()
+////                                .anyRequest().authenticated())
+////                .httpBasic(httpSecurityHttpBasicConfigurer -> httpSecurityHttpBasicConfigurer.authenticationEntryPoint(authenticationEntryPoint));
+////        http.addFilterAfter(new CustomFilter(), BasicAuthenticationFilter.class);
 //        return http.build();
-//    }
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exeption{
-//
 //    }
 //
 //}
